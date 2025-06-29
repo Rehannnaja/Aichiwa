a57f060ac536b962d35ff35da1bc06924ca1a0ab
!Taka dev <kontol543215@gmail.com>
2025-06-30 03:35:51 +0700

    Update useUser.ts

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,1 +1,14 @@
+import { useAuth } from "@/context/AuthContext";
+
+export default function useUser() {
+  const { user, token, login, logout } = useAuth();
+
+  return {
+    user,
+    token,
+    isLoggedIn: !!user && !!token,
+    login,
+    logout,
+  };
+}
 
