5f489725c7d16e705af0d09e7506ca32080177b1
!Taka dev <kontol543215@gmail.com>
2025-06-30 03:38:28 +0700

    Update anilist.ts

--- lib/anilist.ts
+++ lib/anilist.ts
@@ -1,1 +1,21 @@
+const ANILIST_API = "https://graphql.anilist.co";
+
+export async function fetchAnilist(query: string, variables?: Record<string, any>) {
+  const res = await fetch(ANILIST_API, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+
+  if (!res.ok) throw new Error("Gagal fetch dari AniList");
+
+  const json = await res.json();
+  return json.data;
+}
 
