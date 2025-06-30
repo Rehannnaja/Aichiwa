43923299be8dbbcc42dcac4cc1f02f64c9c4956c
!Taka dev <kontol543215@gmail.com>
2025-06-30 07:48:59 +0700

    Update ManhwaCard.tsx

--- components/ManhwaCard.tsx
+++ components/ManhwaCard.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { BookmarkButton } from "./BookmarkButton";
+import BookmarkButton from "./BookmarkButton";
 
 interface ManhwaCardProps {
   id: string;
