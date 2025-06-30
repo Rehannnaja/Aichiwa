4cd9e46090d2171945e3c402b4f03334356e2ef3
!Taka dev <kontol543215@gmail.com>
2025-06-30 09:54:05 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { AuthProvider } from "@/context/AuthContext"; // pastikan path ini sesuai struktur project kamu
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
